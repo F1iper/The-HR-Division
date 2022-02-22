1fb268b5cbbdd4bd73832e6914ecf8bedef480ba
Filip <coderaligator@gmail.com>
2022-02-22 16:03:56 +0100

    Initial project version

---
+++ src/main/java/com/thehrdivision/TheHrDivisionApplication.java
@@ -0,0 +1,13 @@
+package com.thehrdivision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHrDivisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHrDivisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thehrdivision/TheHrDivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehrdivision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHrDivisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
