5e0d7b1dd3422d670ae78c3be99e0fe64371125b
dylionen <adamosokolos@gmail.com>
2022-03-09 18:39:30 +0100

    Added Lombok getters/setters and created User Entity

---
+++ src/main/java/com/thehrdivision/Employee.java
@@ -0,0 +1,13 @@
+package com.thehrdivision;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+}
