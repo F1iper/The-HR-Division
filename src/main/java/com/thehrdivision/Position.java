91004ac7730a7c7c77e2a3007c396f98cb163d77
dylionen <41701026+dylionen@users.noreply.github.com>
2022-03-12 16:00:29 +0100

    Third 5 (#11)
    * Added Lombok getters/setters and created User Entity
    
    * Corrected variable types.

---
+++ src/main/java/com/thehrdivision/Position.java
@@ -0,0 +1,12 @@
+package com.thehrdivision;
+
+import javax.persistence.*;
+
+@Entity
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+
+}
---
+++ src/main/java/com/thehrdivision/UserEntity.java
@@ -0,0 +1,44 @@
+package com.thehrdivision;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String workEmail;
+
+    @OneToOne()
+    @JoinColumn(name = "position_id")
+    private Position position;
+
+    @Column(nullable = false, updatable = false)
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    @OneToOne
+    @JoinColumn(name = "employee_id")
+    private Employee employee;
+
+}
