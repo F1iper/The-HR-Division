07e1e413f5a3861e3f08c8d54aad242f1ffa7c50
Arkadiusz Swioklo <swiokloarkadiusz@gmail.com>
2022-03-24 21:48:20 +0100

    test

--- src/main/java/com/thehrdivision/components/AuthorizationConfiguration.java
+++ src/main/java/com/thehrdivision/components/AuthorizationConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthorizationConfiguration {
-
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
